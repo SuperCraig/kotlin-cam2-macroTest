e9bf49fdb5c42a33e28fbd80876e41d00a75c51e
SuperCraigGG <SuperCraigGG@bitbucket.org>
2020-03-19 18:07:02 +0800

    first

---
+++ app/src/main/java/com/example/cargicamera2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.cargicamera2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        savedInstanceState ?: supportFragmentManager.beginTransaction()
+            .replace(R.id.container, Camera2BasicFragment.newInstance()).commit()
+    }
+}
